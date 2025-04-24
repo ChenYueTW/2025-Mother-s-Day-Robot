6c399ea591deb683de24a9ee0181a34c3c5a141e
ChenYueTW <eagleh2005@gmail.com>
2025-04-24 23:21:15 +0800

    Rebuild v0.6

--- src/main/java/frc/robot/subsystems/RebuildVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/RebuildVisionSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import frc.robot.lib.helpers.RebuildPhotonHelper;
 import frc.robot.lib.helpers.RebuildPhotonHelper.EstimateConsumer;
 import frc.robot.lib.subsystems.SubsystemBase;
@@ -14,13 +15,13 @@ public class RebuildVisionSubsystem extends SubsystemBase {
         super("RebuildVision");
         this.leftCamera = new RebuildPhotonHelper(
             "left-Cam",
-            new Translation3d(0, 0, 0),
-            new Rotation3d(0, 0, 0),
+            new Translation3d(0.261372, 0.236374, 0.224837),
+            new Rotation3d(0.0, Units.degreesToRadians(10.0), Units.degreesToRadians(-15.0)),
             estimateConsumer);
         this.rightCamera = new RebuildPhotonHelper(
             "right-Cam",
-            new Translation3d(0, 0, 0),
-            new Rotation3d(0, 0, 0),
+            new Translation3d(0.261372, -0.236374, 0.224837),
+            new Rotation3d(0.0, Units.degreesToRadians(10.0), Units.degreesToRadians(15.0)),
             estimateConsumer);
     }
 
